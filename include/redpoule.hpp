72cf829bc4f2f823691d1231fef8ceb997c76e90
Robin Scheibler <fakufaku@gmail.com>
2020-08-19 14:35:29 +0900

    Adds condition variable. Fixes some races.

--- include/redpoule.hpp
+++ include/redpoule.hpp
@@ -21,8 +21,15 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+#ifndef __REDPOULE_H__
+#define __REDPOULE_H__
+
+#include <unistd.h>
+
 #include <algorithm>
+#include <condition_variable>
 #include <functional>
+#include <iostream>
 #include <mutex>
 #include <queue>
 #include <thread>
@@ -38,25 +45,28 @@ class ThreadPool {
   bool _running = false;
 
   // attributes
-  int _n_threads = 0;                             // number of threads
-  std::vector<std::thread> _threads;              // the threads in the pool
-  std::mutex _task_queue_mutex;                   // mutex for the task queue
+  int _n_threads = 0;                 // number of threads
+  std::vector<std::thread> _workers;  // the threads in the pool
+  std::vector<bool>
+      _busy;  // flag that indicates if the threads are working or not
+  std::mutex _task_queue_mutex;  // mutex for the task queue
+  std::condition_variable cv;    // get notification when the queue is empty
   std::queue<std::function<void()>> _task_queue;  // the task queue
 
   // private methods
-  void infinite_loop();
+  void infinite_loop(int thread_id);
 
  public:
   ThreadPool(size_t n_threads) : _running(true), _n_threads(n_threads) {
-    _threads.resize(_n_threads);
+    _busy.resize(_n_threads, false);
     for (int t = 0; t < _n_threads; t++)
-      _threads[t] = std::thread(&ThreadPool::infinite_loop, this);
+      _workers.emplace_back(std::thread(&ThreadPool::infinite_loop, this, t));
   }
   ThreadPool() : ThreadPool(max_n_threads) {}
   ~ThreadPool() {
     _running = false;
 
-    std::for_each(_threads.begin(), _threads.end(),
+    std::for_each(_workers.begin(), _workers.end(),
                   [](std::thread &x) { x.join(); });
   }
 
@@ -66,39 +76,40 @@ class ThreadPool {
   void push(std::function<void()> &&task);
   template <class Func>
   void parallel_for(size_t start, size_t end, Func &&f);
+  void wait() {
+    // wait for all tasks to be finished
+    std::unique_lock<std::mutex> lock(_task_queue_mutex);
+    cv.wait(lock, [this] {
+      return _task_queue.empty() && std::none_of(_busy.begin(), _busy.end(), [](bool v) { return v; });
+    });
+  }
 };
 
 void ThreadPool::push(std::function<void()> &&task) {
   /*
    * Push a new task into the queue
    */
-  std::unique_lock<std::mutex> lock(_task_queue_mutex);
+  std::lock_guard<std::mutex> lock(_task_queue_mutex);
   _task_queue.push(std::forward<std::function<void()>>(task));
 }
 
 template <class Func>
 void ThreadPool::parallel_for(size_t start, size_t end, Func &&f) {
-
-  if (start >= end)
-    return;
+  if (start >= end) return;
 
   for (size_t t = 0; t < get_num_threads(); t++) {
     size_t block_size = end - start;
     size_t block_start = t * block_size / get_num_threads();
     size_t block_end = (t + 1) == get_num_threads()
-                      ? block_size
-                      : (t + 1) * block_size / get_num_threads();
+                           ? block_size
+                           : (t + 1) * block_size / get_num_threads();
     push(std::bind(std::forward<Func>(f), block_start, block_end));
   }
 
-  // busy wait
-  // TODO replace with condition variable
-  // https://en.cppreference.com/w/cpp/thread/condition_variable
-  while (_task_queue.size() > 0)
-    ;
+  wait();
 }
 
-void ThreadPool::infinite_loop() {
+void ThreadPool::infinite_loop(int thread_id) {
   /*
    * The infinite loop run by the worker threads
    */
@@ -106,15 +117,28 @@ void ThreadPool::infinite_loop() {
   while (_running) {
     // get new task
     {
-      std::unique_lock<std::mutex> lock(_task_queue_mutex);
-      if (_task_queue.size() == 0) continue;
+      std::lock_guard<std::mutex> lock(_task_queue_mutex);
+      if (_task_queue.size() == 0) {
+        _busy[thread_id] = false;
+        continue;
+      }
+
       task = std::move(_task_queue.front());
       _task_queue.pop();
+      _busy[thread_id] = true;
     }
 
-    // run it
+    // execute the task
     task();
+
+    // notify that we finished one job
+    {
+      std::lock_guard<std::mutex> lock(_task_queue_mutex);
+      _busy[thread_id] = false;
+      cv.notify_all();
+    }
   }
 }
 
 }  // namespace redpoule
+#endif
